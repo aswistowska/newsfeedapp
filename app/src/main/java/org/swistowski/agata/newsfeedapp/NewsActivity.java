05cd64f2c955a986fc98b32e8507d7d8f5b4f4f9
Agata <agata@swistowski.org>
2018-04-10 14:31:09 +0100

    Making a Preference Screen

--- app/src/main/java/org/swistowski/agata/newsfeedapp/NewsActivity.java
+++ app/src/main/java/org/swistowski/agata/newsfeedapp/NewsActivity.java
@@ -9,6 +9,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -109,5 +111,24 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             TextView NoInternetTextView = (TextView) findViewById(R.id.no_internet_access);
             NoInternetTextView.setText(R.string.no_internet_access);
         }
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
